6bab09a10ca1ad33fcc3976a46c580a861248d54
Jake Kealey <jake.kealey@nhs.net>
2024-09-09 13:13:44 +0100

    Added dedicated runtime settings as an output and organised function by grouping reused variables into a common dict.

--- src/utils/global_params.py
+++ src/utils/global_params.py
@@ -2,12 +2,44 @@ import datetime
 from os import getenv
 from dotenv import load_dotenv
 
+#Function to load environmental settings
 def import_settings(config, ds):
 
     load_dotenv(override=True)
 
+    #Load runtime settings seperately
+    if ds == "runtime":
+        params_runtime ={
+            "DATA_PROCESSING_SMARTAPI": (
+                    {"True": True, "False": False}
+                    [getenv("DATA_PROCESSING_SMARTAPI")]),
+            "DATA_PROCESSING_LASHANDOVER": (
+                    {"True": True, "False": False}
+                    [getenv("DATA_PROCESSING_LASHANDOVER")]),
+            "DATA_PROCESSING_LIVETRACKERS": (
+                    {"True": True, "False": False}
+                    [getenv("DATA_PROCESSING_LIVETRACKERS")]),
+            "DATA_EXPORT": (
+                    {"True": True, "False": False}
+                    [getenv("DATA_EXPORT")])
+        }
+        return params_runtime
+    
+    else:
+        #Load parameters common to all datasets
+        params_ds_global ={
+            "SQL_ADDRESS": getenv("SQL_ADDRESS"),
+            "SQL_DATABASE": config["database"]["sql_database"],
+            "SQL_SCHEMA": config["database"]["sql_schema"],
+            "SQL_TABLE_PREFIX": config["database"]["sql_table_prefix"],
+            "SQL_TABLE_SUFFIX": config["database"]["sql_table_suffix"],
+
+            "WAIT_COOLOFF": config["database"]["sql_cooloff"]
+        }
+
+    #Load the dataset specific parameters
     if ds == "smart_api":
-        params_smart ={
+        params_ds_unique ={
                 "API_URL": config["smart_api"]["base"]["api_url"],
                 "API_KEY": getenv("SMART_API_KEY"),
                 "SITES": config["smart_api"]["base"]["sites"],
@@ -16,49 +48,25 @@ def import_settings(config, ds):
                 "DATE_WINDOW": config["smart_api"]["date"]["window"],
                 
                 "WAIT_PERIOD": config["smart_api"]["wait"]["period"],
-                "WAIT_COOLOFF": config["database"]["sql_cooloff"],
 
-                "SQL_ADDRESS": getenv("SQL_ADDRESS"),
-                "SQL_DATABASE": config["database"]["sql_database"],
-                "SQL_SCHEMA": config["database"]["sql_schema"],
-                "SQL_TABLE": config["smart_api"]["base"]["sql_table"],
+                "SQL_TABLE": config["smart_api"]["base"]["sql_table"]
             }
-        return params_smart
     
     elif ds == "las":
-        params_las ={
-                "SQL_ADDRESS": getenv("SQL_ADDRESS"),
-                "SQL_DATABASE": config["database"]["sql_database"],
-                "SQL_SCHEMA": config["database"]["sql_schema"],
-                "SQL_TABLE": config["las"]["base"]["sql_table"],
-                "WAIT_COOLOFF": config["database"]["sql_cooloff"],
+        params_ds_unique ={
                 "ARCHIVE_LAS": config["las"]["base"]["archive_file"],
-                "EXTRACT_NUMBER_OF_DAYS": config["las"]["base"]["extract_number_of_days"]
+                "EXTRACT_NUMBER_OF_DAYS": config["las"]["base"]["extract_number_of_days"],
+
+                "SQL_TABLE": config["las"]["base"]["sql_table"]
             }
-        return params_las
-    
-    elif ds == "ecist":
-        params_ecist ={
-                "SQL_ADDRESS": getenv("SQL_ADDRESS"),
-                "SQL_DATABASE": config["database"]["sql_database"],
-                "SQL_SCHEMA": config["database"]["sql_schema"],
-                "SQL_TABLE": config["ecist_sitrep"]["base"]["sql_table"],
-                "WAIT_COOLOFF": config["database"]["sql_cooloff"],
-                "ARCHIVE_ECIST": config["ecist_sitrep"]["base"]["archive_file"]
-            }
-        return params_ecist
     
     elif ds == "live_tracker":
-        params_tracker ={
+        params_ds_unique ={
                 "ARCHIVE_FILE": config["live_tracker"]["archive_file"],
 
                 "mo" : config["live_tracker"]["mo"]["mo"],
                 "p2" : config["live_tracker"]["p2"]["p2"],
                 "vw" : config["live_tracker"]["vw"]["vw"],
-
-                "SQL_ADDRESS": getenv("SQL_ADDRESS"),
-                "SQL_DATABASE": config["database"]["sql_database"],
-                "SQL_SCHEMA": config["database"]["sql_schema"],
 
                 "MO_SHEET_NAME": config["live_tracker"]["mo"]["mo_sheet_name"],
                 "MO_TABLE_HEADING": config["live_tracker"]["mo"]["mo_table_heading"],
@@ -72,9 +80,11 @@ def import_settings(config, ds):
                 "VW_SHEET_NAME": config["live_tracker"]["vw"]["vw_sheet_name"],
                 "VW_SQL_TABLE": config["live_tracker"]["vw"]["vw_sql_table"]
             }
-        params_tracker["date_extract"] = datetime.date.today().strftime("%Y-%m-%d")
+        params_ds_unique["date_extract"] = datetime.date.today().strftime("%Y-%m-%d")
 
-        return params_tracker
-    
+    #Raise error if dataset given is not listed above
     else:
         raise ValueError (f"{ds} is not supported.")
+    
+    #Return the combination of the global and dataset specific parameters
+    return params_ds_global | params_ds_unique
